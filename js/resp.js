91f954f42176f531a26af652dd9831bf5cad5c7a
Vishesh Gupta <visheshgupta065@gmail.com>
2024-04-10 11:00:40 +0530

    first commit

--- js/resp.js
+++ js/resp.js
@@ -2,12 +2,14 @@ burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 rightNav = document.querySelector('.rightNav')
 navList = document.querySelector('.nav-list')
+rightNavList =document.querySelector('.right-nav-list')
 
 
 
 burger.addEventListener('click',()=>{
     navbar.classList.toggle('h-nav-resp')
     navList.classList.toggle('v-class-resp')
-    rightNav.classList.toggle('v-class-resp')
+    rightNav.classList.toggle('v-class-resp')   
+    rightNavList.classList.toggle('h-class-resp')
     burger.classList.toggle('burger-top')
 })
